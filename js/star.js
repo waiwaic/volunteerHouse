86885555ca5cb9a71b4d16eb6cab4a3ccca30fa7
yeeing <511158080@qq.com>
2018-05-13 05:39:09 +0800

    Update star.js

--- js/star.js
+++ js/star.js
@@ -1,14 +1,11 @@
 function star(){
   var xmlhttp=new XMLHttpRequest();
-  xmlhttp.onreadystatechange=function()
-    {
-    if (xmlhttp.readyState==4 && xmlhttp.status==200)
-      {
+  xmlhttp.onreadystatechange=function(){
+    if (xmlhttp.readyState==4 && xmlhttp.status==200){
       document.getElementById("stars").innerHTML=xmlhttp.responseText;
-      }
     }
+  }
   xmlhttp.open("POST","/php/star.php",true);
   xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
   xmlhttp.send("id=1");
-  }
 }
