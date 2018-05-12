12825bc2036c74ced30f347263d65e3e27ea258f
yeeing <511158080@qq.com>
2018-05-12 10:48:35 +0800

    Create pdo.php

---
+++ php/pdo.php
@@ -0,0 +1,9 @@
+<?php
+$dsn = 'mysql:dbname=thehomeofvolunteers;host=10.130.11.146';
+$user = 'userA76';
+$password = '5xHrGB3ScWVYRm5J';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
