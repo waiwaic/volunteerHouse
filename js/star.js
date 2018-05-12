6a2ee88039deff731390015d02e284c7ef8a4827
yeeing <511158080@qq.com>
2018-05-13 05:07:54 +0800

    Update star.js

--- js/star.js
+++ js/star.js
@@ -1,20 +1,14 @@
 function star(){
-var xmlhttp;
-if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-}
-else{// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-}
-xmlhttp.onreadystatechange=function()
-  {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function()
     {
-    document.getElementById("stars").innerHTML=xmlhttp.responseText;
+    if (xmlhttp.readyState==4 && xmlhttp.status==200)
+      {
+      document.getElementById("stars").innerHTML=xmlhttp.responseText;
+      }
     }
+  xmlhttp.open("POST","/php/star.php",true);
+  xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+  xmlhttp.send("id=1");
   }
-xmlhttp.open("POST","/php/star.php",true);
-xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-xmlhttp.send("id=1");
-}
 }
