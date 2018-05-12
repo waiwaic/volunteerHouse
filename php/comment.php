33cb35262153061dea4b7c156a47d1671da826f0
yeeing <511158080@qq.com>
2018-05-12 16:29:34 +0800

    Update comment.php

--- php/comment.php
+++ php/comment.php
@@ -1,3 +1,17 @@
 <?php
-require('dbh.php');
+$dsn = 'mysql:dbname=thehomeofvolunteers;host=10.130.11.146';
+
+$user = 'userA76';
+
+$password = '5xHrGB3ScWVYRm5J';
+
+try {
+
+    $dbh = new PDO($dsn, $user, $password);
+
+} catch (PDOException $e) {
+
+    echo 'Connection failed: ' . $e->getMessage();
+
+}
 $dbh->exec("INSERT INTO comments VALUES($_POST[name],$_POST[comment])");
