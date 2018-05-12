37c1aa6124a5a7a11331744d73e26875075a4adc
yeeing <511158080@qq.com>
2018-05-13 02:51:28 +0800

    Update comment.php

--- php/comment.php
+++ php/comment.php
@@ -15,4 +15,8 @@ if($_GET[id]){
 }
 if($_POST[comment]){
     $dbh->exec("INSERT INTO comments VALUES($_POST[name],$_POST[comment],$_POST[id])");
+    foreach ($dbh->query("SELECT comment FROM comments WHERE id=$_GET[id]") as $row) {
+        echo "<h5>$row[name]</h5>";
+        echo "<p>$row[comment]</p>";
+    }
 }
