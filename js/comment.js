74f545aaa62091ebfd404b4a2b37fe707a3ba478
yeeing <511158080@qq.com>
2018-05-13 06:50:15 +0800

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -7,5 +7,5 @@ xmlhttp.onreadystatechange=function(){
 }
 xmlhttp.open("POST","/php/comment.php",true);
 xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-xmlhttp.send(name=document.getElementById("name").innerHTML&comment=document.getElementById("comments").innerHTML);
+xmlhttp.send(name=document.getElementById("name").value&comment=document.getElementById("comments").value);
 }
