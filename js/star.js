b78efb9d5e26124f4c1aac421071b5b0d23e1bc5
yeeing <511158080@qq.com>
2018-05-13 04:51:16 +0800

    Update star.js

--- js/star.js
+++ js/star.js
@@ -8,14 +8,13 @@ else{// code for IE6, IE5
 }
 xmlhttp.onreadystatechange=function()
   {
-/*  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
     {
     document.getElementById("stars").innerHTML=xmlhttp.responseText;
     }
-  }*/
+  }
 xmlhttp.open("POST","/php/star.php",true);
 xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-xmlhttp.send("star=1");
-document.getElementById("stars").innerHTML=(parseInt(document.getElementById("stars").innerHTML)+1).toString();
+xmlhttp.send("id=1");
 }
 }
