f325f407df32aa458040c424c65f07e28a150a1b
yeeing <511158080@qq.com>
2018-05-12 14:59:34 +0800

    Create star.php

---
+++ php/star.php
@@ -0,0 +1,8 @@
+<?php
+require('pdo.php');
+if($_GET[star]){
+  echo dbh->query("SELECT star FROM stars WHERE id=0")[0];
+}
+if($_POST[star]){
+  dbh->exec("UPDATE stars SET num=num+1 WHERE id=$_POST[id]");
+}
