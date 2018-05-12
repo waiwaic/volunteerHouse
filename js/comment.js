ae0b9f20328d015ba523d408731fe6840305cc80
yeeing <511158080@qq.com>
2018-05-12 14:14:23 +0800

    Create comment.js

---
+++ js/comment.js
@@ -0,0 +1,20 @@
+function loadXMLDoc(){
+var xmlhttp;
+if (window.XMLHttpRequest){
+    //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+    xmlhttp=new XMLHttpRequest();
+}
+else{
+    // IE6, IE5 浏览器执行代码
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+}
+xmlhttp.onreadystatechange=function(){
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("comments").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("GET","/php/comments.php",true);
+xmlhttp.send();
+}
+
