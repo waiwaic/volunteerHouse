93382f8ded14cced18b76d844e07056bd5b023cc
yeeing <511158080@qq.com>
2018-05-12 17:44:37 +0800

    Update comment.php

--- php/comment.php
+++ php/comment.php
@@ -1,17 +1,18 @@
 <?php
 $dsn = 'mysql:dbname=thehomeofvolunteers;host=10.130.11.146';
-
 $user = 'userA76';
-
 $password = '5xHrGB3ScWVYRm5J';
-
 try {
-
     $dbh = new PDO($dsn, $user, $password);
-
 } catch (PDOException $e) {
-
     echo 'Connection failed: ' . $e->getMessage();
-
 }
-$dbh->exec("INSERT INTO comments VALUES($_POST[name],$_POST[comment])");
+if($_GET){
+    foreach ($dbh->query("SELECT comment FROM comments WHERE id=$_GET[id]") as $row) {
+        echo "<h5>$row[name]</h5>";
+        echo "<p>$row[comment]</p>";
+    }
+}
+if($_POST){
+    $dbh->exec("INSERT INTO comments VALUES($_POST[name],$_POST[comment])");
+}
