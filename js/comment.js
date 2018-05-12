c49895975a2c26541bb07e430f3151457e3dbb64
yeeing <511158080@qq.com>
2018-05-12 18:07:42 +0800

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -8,6 +8,13 @@ else{
     // IE6, IE5 浏览器执行代码
     xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
 }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("comments").innerHTML=xmlhttp.responseText;
+    }
+  }
 var name=document.getElementById("name").innerHTML;
 var comment=document.getElementById("comment").innerHTML;
 xmlhttp.open("POST","/php/comment.php",true);
