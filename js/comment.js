a42bcc31dff715d7a19f435289073e284e13ee56
yeeing <511158080@qq.com>
2018-05-13 05:08:38 +0800

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -1,20 +1,10 @@
 function comments(){
-var xmlhttp;
-if (window.XMLHttpRequest){
-    //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
-    xmlhttp=new XMLHttpRequest();
-}
-else{
-    // IE6, IE5 浏览器执行代码
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-}
-xmlhttp.onreadystatechange=function()
-  {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
+var xmlhttp=new XMLHttpRequest();
+xmlhttp.onreadystatechange=function(){
+  if (xmlhttp.readyState==4 && xmlhttp.status==200){
     document.getElementById("comments").innerHTML=xmlhttp.responseText;
-    }
   }
+}
 xmlhttp.open("POST","/php/comment.php",true);
 xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 xmlhttp.send(name=document.getElementById("name").innerHTML&comment=document.getElementById("comments").innerHTML&id=2);
