1e3f79d37fcfe531e9bc6a37688901505670b82f
yeeing <511158080@qq.com>
2018-05-13 03:01:23 +0800

    Update comment.php

--- php/comment.php
+++ php/comment.php
@@ -7,12 +7,6 @@ try {
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
-if($_GET[id]){
-    foreach ($dbh->query("SELECT comment FROM comments WHERE id=$_GET[id]") as $row) {
-        echo "<h5>$row[name]</h5>";
-        echo "<p>$row[comment]</p>";
-    }
-}
 if($_POST[comment]){
     $dbh->exec("INSERT INTO comments VALUES($_POST[name],$_POST[comment],$_POST[id])");
     foreach ($dbh->query("SELECT comment FROM comments WHERE id=$_GET[id]") as $row) {
