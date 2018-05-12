7edf2a4dd7392308b4af96abca5129e254e9d3f5
yeeing <511158080@qq.com>
2018-05-12 17:50:35 +0800

    Update star.php

--- php/star.php
+++ php/star.php
@@ -1,22 +1,15 @@
 <?php
 $dsn = 'mysql:dbname=thehomeofvolunteers;host=10.130.11.146';
-
 $user = 'userA76';
-
 $password = '5xHrGB3ScWVYRm5J';
-
 try {
-
     $dbh = new PDO($dsn, $user, $password);
-
 } catch (PDOException $e) {
-
     echo 'Connection failed: ' . $e->getMessage();
-
 }
-if($_GET[star]){
-  echo $dbh->query("SELECT star FROM stars WHERE id=0")[0];
+if($_GET){
+  echo $dbh->query("SELECT num FROM stars WHERE id=0");
 }
-if($_POST[star]){
+if($_POST){
   $dbh->exec("UPDATE stars SET num=num+1 WHERE id=$_POST[id]");
 }
