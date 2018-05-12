4ef174ea267742cbc611d8e3c43349164a01f724
yeeing <511158080@qq.com>
2018-05-12 18:04:01 +0800

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -1,4 +1,4 @@
-function get_comments(){
+function comments(){
 var xmlhttp;
 if (window.XMLHttpRequest){
     //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
@@ -8,12 +8,9 @@ else{
     // IE6, IE5 浏览器执行代码
     xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
 }
-xmlhttp.onreadystatechange=function(){
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-    document.getElementById("comments").innerHTML=xmlhttp.responseText;
-    }
-  }
-xmlhttp.open("GET","/php/comments.php",true);
-xmlhttp.send();
+var name=document.getElementById("name").innerHTML;
+var comment=document.getElementById("comment").innerHTML;
+xmlhttp.open("POST","/php/comment.php",true);
+xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+xmlhttp.send(name=document.getElementById("name").innerHTML&comment=document.getElementById("comments").innerHTML);
 }
