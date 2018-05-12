d844cd26e57c06d187eee31c6093b3bb695148bf
yeeing <511158080@qq.com>
2018-05-12 18:08:00 +0800

    Update star.js

--- js/star.js
+++ js/star.js
@@ -10,7 +10,7 @@ xmlhttp.onreadystatechange=function()
   {
   if (xmlhttp.readyState==4 && xmlhttp.status==200)
     {
-    document.getElementById("star").innerHTML=xmlhttp.responseText;
+    document.getElementById("stars").innerHTML=xmlhttp.responseText;
     }
   } 
 xmlhttp.open("POST","/php/star.php",true);
